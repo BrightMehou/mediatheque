5b726f07f8a72f2ce5e5475f1fd4e67996855261
BrightMehou <joanemehou@gmail.com>
2026-08-08 22:48:28 +0200

    fie trigger

--- database/create_table.sql
+++ database/create_table.sql
@@ -1,22 +1,16 @@
-CREATE OR REPLACE FUNCTION set_audit_timestamps()
-RETURNS trigger AS $$
-BEGIN
-    IF TG_OP = 'INSERT' THEN
-        IF NEW.created_at IS NULL THEN
-            NEW.created_at = now();
-        END IF;
-        NEW.updated_at = now();
-    ELSIF TG_OP = 'UPDATE' THEN
-        NEW.updated_at = now();
-    END IF;
-    RETURN NEW;
+CREATE
+OR REPLACE FUNCTION set_updated_at() RETURNS trigger AS $ $ BEGIN NEW.updated_at = NOW();
+
+RETURN NEW;
+
 END;
-$$ LANGUAGE plpgsql;
+
+$ $ LANGUAGE plpgsql;
 
 CREATE TABLE IF NOT EXISTS author (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    created_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz NOT NULL DEFAULT now(),
+    created_at timestamptz NOT NULL DEFAULT NOW(),
+    updated_at timestamptz NOT NULL DEFAULT NOW(),
     deleted_at timestamptz,
     first_name text,
     last_name text,
@@ -25,16 +19,16 @@ CREATE TABLE IF NOT EXISTS author (
 
 CREATE TABLE IF NOT EXISTS book_type (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    created_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz NOT NULL DEFAULT now(),
+    created_at timestamptz NOT NULL DEFAULT NOW(),
+    updated_at timestamptz NOT NULL DEFAULT NOW(),
     deleted_at timestamptz,
-    type text NOT NULL UNIQUE
+    TYPE text NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS book (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    created_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz NOT NULL DEFAULT now(),
+    created_at timestamptz NOT NULL DEFAULT NOW(),
+    updated_at timestamptz NOT NULL DEFAULT NOW(),
     deleted_at timestamptz,
     author_id integer REFERENCES author(id) NOT NULL,
     title text NOT NULL,
@@ -44,17 +38,20 @@ CREATE TABLE IF NOT EXISTS book (
     page_count integer
 );
 
-DROP TRIGGER IF EXISTS set_audit_timestamps_on_author ON author;
-CREATE TRIGGER set_audit_timestamps_on_author
-BEFORE INSERT OR UPDATE ON author
-FOR EACH ROW EXECUTE FUNCTION set_audit_timestamps();
+DROP TRIGGER IF EXISTS set_updated_at_on_author ON author;
 
-DROP TRIGGER IF EXISTS set_audit_timestamps_on_book_type ON book_type;
-CREATE TRIGGER set_audit_timestamps_on_book_type
-BEFORE INSERT OR UPDATE ON book_type
-FOR EACH ROW EXECUTE FUNCTION set_audit_timestamps();
+CREATE TRIGGER set_updated_at_on_author BEFORE
+UPDATE
+    ON author FOR EACH ROW EXECUTE FUNCTION set_updated_at();
 
-DROP TRIGGER IF EXISTS set_audit_timestamps_on_book ON book;
-CREATE TRIGGER set_audit_timestamps_on_book
-BEFORE INSERT OR UPDATE ON book
-FOR EACH ROW EXECUTE FUNCTION set_audit_timestamps();
+DROP TRIGGER IF EXISTS set_updated_at_on_book_type ON book_type;
+
+CREATE TRIGGER set_updated_at_on_book_type BEFORE
+UPDATE
+    ON book_type FOR EACH ROW EXECUTE FUNCTION set_updated_at();
+
+DROP TRIGGER IF EXISTS set_updated_at_on_book ON book;
+
+CREATE TRIGGER set_updated_at_on_book BEFORE
+UPDATE
+    ON book FOR EACH ROW EXECUTE FUNCTION set_updated_at();
